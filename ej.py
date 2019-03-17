92fa1838cacc721551ddb46e12739530496384b3
s-nt-s <santos82h@gmail.com>
2019-03-17 22:59:41 +0100

    Seleccionar ejercicios e imprimir resultados

--- ej.py
+++ ej.py
@@ -7,12 +7,21 @@ import sys
 import re
 #|Problemas para resolver
 re_remove = re.compile(r"^\s*(Ejercicios para practicar|Actividades para pensar más|Encuentra el error)\s*$")
+re_trim = re.compile(r'\s*\(<span class="color2">[●○]+</span>\s*\)\s*</p>')
 heads = ["h1", "h2", "h3", "h5", "h6"]
 
 def get_soup(h):
     with open(h, "r") as f:
         soup = bs4.BeautifulSoup(f, "lxml")
         return soup
+
+def add_class(n, cl):
+    c = n.attrs.get("class", "")
+    if isinstance(c, str):
+        c = (c + " "+cl).strip()
+    else:
+        c.append(cl)
+    n.attrs["class"]=c
 
 def get_tpt(title):
     soup = bs4.BeautifulSoup('''
@@ -21,7 +30,7 @@ def get_tpt(title):
 	<head>
 		<title>%s</title>
 		<meta charset="utf-8"/>
-        <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script type="text/javascript" async src="../m/MathJax-2.7.2/MathJax.js?config=TeX-AMS_CHTML"></script>
         <script type="text/x-mathjax-config">
           MathJax.Hub.Config({
@@ -47,10 +56,15 @@ def get_tpt(title):
         </script>
         <link href="../m/theme.css" rel="stylesheet" type="text/css">
         <link href="../m/theme_ej.css" rel="stylesheet" type="text/css">
-        <!--script type="text/javascript" async src="../m/ej.js"></script-->
+        <script type="text/javascript" async src="../m/ej.js"></script>
 	</head>
-	<body contenteditable="true">
-
+	<body>
+        <div>
+        <h1 contenteditable="true">Escriba aquí el título de la ficha</h1>
+        <p contenteditable="true">Escriba aquí la descripción de la ficha</p>
+        </div>
+        <div contenteditable="true">
+        </div>
 	</body>
 </html>
 	'''.strip() % (title)
@@ -62,6 +76,7 @@ for h in sorted(glob("out/*_-_*.html")):
     soup=get_soup(h)
     title = soup.find("title").get_text().strip()
     ej = get_tpt(title+" - Ejercicios")
+    body = ej.findAll("div")[-1]
 
     for ep in soup.findAll("div", text=re_remove):
         ep.extract()
@@ -71,28 +86,36 @@ for h in sorted(glob("out/*_-_*.html")):
         if span:
             span.append(". ")
         cap = h1.get_text().strip()
+        if cap in ("Actividades clave", "Ponte a prueba"):
+            continue
         if cap in ("Actividades clave", "Actividades", "Ponte a prueba"):
             if cap == "Actividades":
                 h1.extract()
                 for h3 in div.findAll("h3"):
                     h3.name="h1"
-            ej.append(div)
+            body.append(div)
         else:
             for a in div.select("div.actividades"):
                 a.find("h1").string = cap
-                ej.append(a)
-    for e in ej.select(".exercise, .section"):
+                body.append(a)
+    for e in body.select(".exercise, .section"):
         if e.find("a"):
             e.extract()
-    for i in ej.findAll(heads):
+    for i in body.findAll(heads):
         if len(i.get_text().strip())==0:
             i.extract()
-    for img in ej.findAll("img"):
+        else:
+            add_class(i, "phide")
+
+    for img in body.findAll("img"):
         img.attrs["src"] = "../" + img.attrs["src"]
 
-    '''
-    for p in ej.findAll(heads + ["p"]):
-        p.attrs["contenteditable"]="true"
-    '''
-    with open("out/ej/"+h[4:12]+".html", "w") as file:
-        file.write(str(ej))
+    for e in body.select("div.exercise"):
+        n = e.select("div.sm-exercise-number")[0]
+        n.attrs["contenteditable"]="false"
+        n = n.get_text().strip()
+        add_class(e, "e"+n)
+
+    with open("out/ej/"+h[4:], "w") as file:
+        ej = re_trim.sub("</p>",str(ej))
+        file.write(ej)
--- out/m/ej.js
+++ out/m/ej.js
@@ -1,8 +1,6 @@
-$(document).ready(function() {/*
-  $("h1, h2, h3, h4, h5, h6, p").click(function(){
-    var e=$(this);
-    var isEditable=e.is('.editable');
-    e.prop('contenteditable',!isEditable).toggleClass('editable')
-  })*/
-  //$("h1, h2, h3, h4, h5, h6, p").prop('contenteditable', true);
+$(document).ready(function() {
+  $("div.sm-exercise-number").click(function(){
+    var e = $(this).closest("div.exercise");
+    e.toggleClass("pshow")
+  })
 })
